f853022189332a25f406fe2a1a47a441e077d3af
Evita <evitavarvaryuk8@gmail.com>
2018-09-27 22:37:43 +0300

    NMR-169 added a variable

--- ev/main.cpp
+++ ev/main.cpp
@@ -5,5 +5,7 @@ void hi_i_am_a_funciton() {
 int main() {
     std::cout << " this is my first commited program" << std::endl;
      std::cout << " edit file " << std::endl;
+     int x = 10;
+     cout << x << endl;
     return 1;
 }
