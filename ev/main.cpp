9ce9f16e8cdfb14d2ea4efbca83ba667071cf723
Evita <evitavarvaryuk8@gmail.com>
2018-10-06 22:44:46 +0300

    NMR-446 case18

--- ev/main.cpp
+++ ev/main.cpp
@@ -2,74 +2,165 @@
 using namespace std;
 int main()
 {
-        int v;
-        cout << "Vvedite vozrast : " << endl;
-        cin >> v;
+    int x;
+    cout << "Vvedite cislo :" << endl;
+    cin >> x;
 
-        if (v / 10 == 2){
-            cout << "Twenty ";
-            goto vozrast;
+    if (x / 100 == 1) {
+        cout << "Sto ";
+    }
+
+    if (x / 100 == 2) {
+        cout << "Dvesti ";
+    }
+
+    if (x / 100 == 3) {
+        cout << "Trista ";
+    }
+
+    if (x / 100 == 4) {
+        cout << "Cetyresta ";
+    }
+
+    if (x / 100 == 5) {
+        cout << "Pyatisot ";
+    }
+
+    if (x / 100 == 6) {
+        cout << "Sestisot ";
+    }
+
+    if (x / 100 == 7) {
+        cout << "Semisot ";
+    }
+
+    if (x / 100 == 8) {
+        cout << "Vosemisot ";
+    }
+
+    if (x / 100 == 9) {
+        cout << "Devyatisot ";
+    }
+
+    if (x % 100 == 0) {
+        cout << " ";
+    }
+
+    if (x % 100 == 10) {
+        cout << "Desyati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 11) {
+        cout << "Odinnadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 12) {
+        cout << "Dvenadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 13) {
+        cout << "Trinadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 14) {
+        cout << "Cetyrnadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 15) {
+        cout << "Pyatnadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 16) {
+        cout << "Sestnadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 17) {
+        cout << "Semnadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 18) {
+        cout << "Vosemnadtati " << endl;
+        return 0;
+    }
+
+    if (x % 100 == 19) {
+        cout << "Devyatnadtati " << endl;
+        return 0;
+    }
+
+    if ((x % 100 / 10) == 2) {
+        cout <<"dvadtati ";
+    }
+
+    if ((x % 100 / 10) == 3) {
+        cout <<"tridtati ";
+    }
+
+    if ((x % 100 / 10) == 4) {
+        cout <<"sorok ";
+    }
+
+    if ((x % 100 / 10) == 5) {
+        cout <<"pyatidesyat ";
+    }
+
+    if ((x % 100 / 10) == 6) {
+        cout <<"sestidesyat ";
+    }
+
+    if ((x % 100 / 10) == 7) {
+        cout <<"semidesyat ";
+    }
+
+    if ((x % 100 / 10) == 8) {
+        cout <<"vosemidesyat ";
+    }
+
+    if ((x % 100 / 10) == 9) {
+        cout <<"devyanosto ";
+    }
+
+        if (x % 10 == 1) {
+            cout << "odin" << endl;
         }
 
-        if (v / 10 == 3){
-            cout << "Thirty ";
-            goto vozrast;
+        if (x % 10 == 2) {
+            cout << "dva" << endl;
         }
 
-        if (v / 10 == 4){
-            cout << "Forty ";
-            goto vozrast;
+        if (x % 10 == 3) {
+            cout << "tri" << endl;
         }
 
-        if (v / 10 == 5){
-            cout << "Fifty ";
-            goto vozrast;
+        if (x % 10 == 4) {
+            cout << "cetyre" << endl;
         }
 
-        if (v / 10 == 6){
-            cout << "Sixty ";
-            goto vozrast;
+        if (x % 10 == 5) {
+            cout << "pyati" << endl;
         }
 
-    vozrast:
-        if (v % 10 == 0) {
-            cout << " " << endl;
+        if (x % 10 == 6) {
+            cout << "sesti" << endl;
         }
 
-        if (v % 10 == 1) {
-            cout << "one" << endl;
+        if (x % 10 == 7) {
+            cout << "semi" << endl;
         }
 
-        if (v % 10 == 2) {
-            cout << "two" << endl;
+        if (x % 10 == 8) {
+            cout << "vosemi" << endl;
         }
 
-        if (v % 10 == 3) {
-            cout << "three" << endl;
-        }
-
-        if (v % 10 == 4) {
-            cout << "four" << endl;
-        }
-
-        if (v % 10 == 5) {
-            cout << "five" << endl;
-        }
-
-        if (v % 10 == 6) {
-            cout << "six" << endl;
-        }
-
-        if (v % 10 == 7) {
-            cout << "seven" << endl;
-        }
-
-        if (v % 10 == 8) {
-            cout << "eight" << endl;
-        }
-
-        if (v % 10 == 9) {
-            cout << "nine" << endl;
+        if (x % 10 == 9) {
+            cout << "devyati" << endl;
         }
 }
-
