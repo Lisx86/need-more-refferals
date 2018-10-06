9e02b08f385f56d894a3888335e7eb2df9f142bd
Evita <evitavarvaryuk8@gmail.com>
2018-10-06 22:38:27 +0300

    NMR-446 case14

--- ev/main.cpp
+++ ev/main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Vvedite storonu treugolinica: ";
+    cin >> a;
 
-void hi_i_am_a_funciton() {
+    int r1 = a*sqrt(3)/6;
+    int r2 = 2*r1;
+    int s = a*a*sqrt(3)/4;
+
+    int x;
+    cout << "Vyberite deistvie: 1, 2, 3, 4" << endl;
+    cin >> x;
+
+switch(x){
+case 1:
+    {
+        cout << "Storona treugolinica = " << a << endl;
+        cout << "Radius vpisannoi okrujnosti = " << r1 << endl;
+        cout << "Radius opisannoi okrujnosti = " << r2 << endl;
+        cout << "Ploseadi treugolinica = " << s << endl;
+         break;
+    }
+
+case 2:
+    {
+        cout << "Radius vpisannoi okrujnosti = " << r1 << endl;
+        cout << "Radius opisannoi okrujnosti = " << r2 << endl;
+        cout << "Ploseadi treugolinica = " << s << endl;
+         break;
+    }
+
+case 3:
+    {
+        cout << "Radius opisannoi okrujnosti = " << r2 << endl;
+        cout << "Ploseadi treugolinica = " << s << endl;
+         break;
+    }
+
+case 4:
+    {
+         cout << "Ploseadi treugolinica = " << s << endl;
+          break;
+    }
+default:
+    cout << "Vy vveli nevernoe znacenie" << endl;
 }
-int main() {
-    std::cout << " this is my first commited program" << std::endl;
-     std::cout << " edit file " << std::endl;
-     int x = 10;
-     cout << x << endl;
-    return 1;
 }
