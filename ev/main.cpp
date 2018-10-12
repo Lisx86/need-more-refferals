e6c6825d3ebeed54662facfdcd8184bc10e3e5d8
Evita <evitavarvaryuk8@gmail.com>
2018-10-12 18:59:54 +0300

    NMR-458 for19

--- ev/main.cpp
+++ ev/main.cpp
@@ -2,27 +2,13 @@
 using namespace std;
 int main()
 {
-    int x;
-    cout << "Vvedite cislo : " << endl;
-    cin >> x;
+    int n;
+    cout << "Vvedite n : " << endl;
+    cin >> n;
 
-    if (x == 0) {
-        cout << x << " nulevoe cislo" << endl;
+    float f;
+    for (int i = 1; i <= n; i++){
+        f = i*(i+1);
     }
-
-    if ((x > 0) && (x % 2 != 0)) {
-        cout << x << " polojitelinoe necetnoe cislo" << endl;
-    }
-
-    if ((x < 0) && (x %2 == 0)){
-        cout << x << " otritatelinoe cetnoe cislo" << endl;
-    }
-
-    if ((x > 0) && (x % 2 == 0)) {
-        cout << x << " polojitelinoe cetnoe cislo" << endl;
-    }
-
-    if ((x < 0) && (x %2 != 0)){
-        cout << x << " otritatelinoe necetnoe cislo" << endl;
-    }
+    cout << f << " - factorial" << endl;
 }
