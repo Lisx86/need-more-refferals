d5dac1f47e53290c2797de0280c76be85d0169e5
Evita <evitavarvaryuk8@gmail.com>
2018-10-12 18:55:46 +0300

    NMR-458 if27 (not working)

--- ev/main.cpp
+++ ev/main.cpp
@@ -6,18 +6,17 @@ int main()
     cout << "Vvedite x : " << endl;
     cin >> x;
 
-    if (x <= 0){
-        float fx = -x;
+    if (x < 0) {
+        int fx = 0;
         cout << "fx = " << fx << endl;
     }
 
-    if ((x > 0) && (x < 2)) {
-        float fx = x*x;
-        cout << "fx = " << fx << endl;
+    int fx = 0;
+    for (int x = 0; x <= 100; x++){
+        if (x % 2 == 0){
+                fx = 1;
+        }
     }
+    cout << "fx = " << fx << endl;
+ }
 
-    if (x >= 2) {
-        float fx = 4;
-        cout << "fx = " << fx << endl;
-    }
-}
