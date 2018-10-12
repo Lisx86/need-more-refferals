b99f70b13ebdcaad887dc7d1fb66131eb89d385c
Evita <evitavarvaryuk8@gmail.com>
2018-10-12 18:53:39 +0300

    NMR-458 if26

--- ev/main.cpp
+++ ev/main.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 int main()
 {
-    int x;
+    float x;
     cout << "Vvedite x : " << endl;
     cin >> x;
 
-    if ((x < -2) || (x > 2)){
-        int fx = 2 * x;
+    if (x <= 0){
+        float fx = -x;
         cout << "fx = " << fx << endl;
     }
-    else {
-        int fx = -3 * x;
+
+    if ((x > 0) && (x < 2)) {
+        float fx = x*x;
+        cout << "fx = " << fx << endl;
+    }
+
+    if (x >= 2) {
+        float fx = 4;
         cout << "fx = " << fx << endl;
     }
 }
